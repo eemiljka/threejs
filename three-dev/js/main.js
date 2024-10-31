7624c3c6d2b0b61b13f33371aade3b7ca75e10ae
eemiljka <eemiljka@metropolia.fi>
2024-10-31 12:45:32 +0200

    shoe

--- three-dev/js/main.js
+++ three-dev/js/main.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 
 let container, camera, scene, renderer, rocket, controls;
 
